6a0e4bcd128cb2c8fb7776ee08ef8e1db91403c9
Spencer A. Lockhart <slockhartbiz@gmail.com>
2019-11-02 09:27:06 -0600

    Updated examples.

--- examples/enforce.js
+++ examples/enforce.js
@@ -1,30 +1,55 @@
-(function (waject) {
-    "use strict";
-    var o = waject();
-    o.mk('foo', 'bar', function(value) {
-        console.log("Foo was set!");
-        
-    }, function() { 
-        console.log("Foo was denied accessed!");
-        return false;
-    });
-    
-    console.log(o.foo);
-    //Foo was denied accessed!
-    //undefined
-    
-    o.mk({
-        property: 'baz',
-        value: 'bam!',
-        preset: function(value)
-        {
-            console.log("Blocking baz Overwrite!")
-            return false; //return false to cancel set.
-        }
-    });
-    
-    o.baz = "boom!";
-    //Blocking Baz Overwrite!
-    console.log("o:" + o);// foo is "bar" and baz is "bam!"
-    
-}(waject));
+let waject = require('waject');
+
+// Create a waject.
+let o = waject();
+
+// log when 'foo' is set.
+o.on('set', 'foo', result => {
+  console.log('foo was set!');
+});
+
+// disallow 'get' access for 'foo'
+o.on('get', 'foo', result => {
+  console.log('Read access denied for foo.');
+  result.value = undefined;
+  result.cycle = false;
+});
+
+// setting foo still works
+o.foo = 'secret value of foo.';
+
+// getting foo does not work.
+console.log(o.foo);
+// Read access denied for foo.
+// returns undefined
+
+// set baz
+o.baz = 'bam!';
+
+// disallow 'set' for baz.
+o.on('set', 'baz', result => {
+  console.log('Write access to baz denied.')
+  result.value = result.target.baz;
+  result.cycle = false;
+});
+
+// setting baz does not work.
+o.baz = "boom!";
+//Blocking Baz Overwrite!
+
+// create a getter for properties that don't "exist"
+// Use this to extract the value of foo.
+o.on('get', 'bar', result => {
+  result.value = result.target.foo;
+});
+
+// Get a static copy of all the properties.
+console.log(o['*']);
+// foo is undefined
+// baz is still "bam!"
+// bar is not listed.
+
+
+// We can use bar to get the value of foo.
+console.log(o.bar); 
+// 'secret value of foo.'
--- examples/logger.js
+++ examples/logger.js
@@ -1,22 +1,21 @@
-var o = waject();
-o.mk('foo', 'bar', function(value) {
-	console.log("Foo was set!");
-}, function() { 
-	console.log("Foo was accessed!")
+let waject = require('waject');
+
+let o = waject();
+
+o.foo = 'bar';
+
+// Log all sets
+o.on('set', result => {
+  console.log(`set:${result.key}`);
+});
+
+// Log all gets
+o.on('get', result => {
+  console.log(`get:${result.key}`);
 });
 
 console.log(o.foo);
-//Foo was accessed!
-//bar
 
-o.mk({
-	property: 'baz',
-	value: 'bam!',
-	preset: function(value) {
-		console.log("Overwriting baz!")
-	}
-});
+o.baz = 'boom!';
 
-o.baz = "boom!";
-//Overwriting baz!
-console.log("o:" + o);// foo is "bar" and baz is "boom!"  
+console.log(o);
